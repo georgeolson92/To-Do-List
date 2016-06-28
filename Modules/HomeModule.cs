01a5de883dae602f3a7d9b4867e46132fa12e627
George Olson and Wolfgang Warneke <george+wolfgang@>
2016-06-28 11:15:09 -0700

    add basic adding and clearing functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,5 +1,6 @@
 using Nancy;
 using ToDoList.Objects;
+using System.Collections.Generic;
 
 namespace ToDoList
 {
@@ -9,8 +10,17 @@ namespace ToDoList
     {
       Get["/"] = _ => View["add_new_task.cshtml"];
       Get["/view_all_tasks"] = _ => {
-        Task newTask = new Task(Request.Query["new-task"]);
-        return View["view_all_tasks.cshtml", newTask];
+        List<string> allTasks = Task.GetAll();
+        return View["view_all_tasks.cshtml", allTasks];
+      };
+      Post["/task_added"] = _ => {
+        Task newTask = new Task (Request.Form["new-task"]);
+        newTask.Save();
+        return View["task_added.cshtml", newTask];
+      };
+      Post["/tasks_cleared"] = _ => {
+        Task.ClearAll();
+        return View["tasks_cleared.cshtml"];
       };
     }
   }
